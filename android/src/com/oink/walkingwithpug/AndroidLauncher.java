bbc0e00989d5a15627d43ee9f2ce1d169841dd34
Sergei Neimushchev <srgynmv@gmail.com>
2016-02-17 15:50:16 +0300

    First commit

---
+++ android/src/com/oink/walkingwithpug/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.oink.walkingwithpug;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.oink.walkingwithpug.PugGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.useImmersiveMode = true;
+
+		initialize(new PugGame(), config);
+	}
+}
